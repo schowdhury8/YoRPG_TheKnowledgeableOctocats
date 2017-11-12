f95baca740ac2c55aaeab4e3976395aafa439c44
ofrank123 <oliverjfrank@yahoo.com>
2017-11-12 18:31:33 -0500

    V2 added

---
+++ v2/Protagonist.java
@@ -0,0 +1,64 @@
+/**
+ * The Knowleadgable Octocats - Oliver Frank, Shayan Chowdhury, Kevin Feng
+ * APCS pd02
+ * HW28 -- Ye Olde Role Playing Game
+ * 2017-11-09
+ **/
+/**
+ * A Protagonist that can fight Monsters. Has attributes like:
+ * <ul>
+ * 	<li>name</li>
+ *  <li>hp</li>
+ * 	<li>strength</li>
+ * 	<li>defense</li>
+ * 	<li>attack rating</li>
+ * </ul>
+ * It can only attack currently. It has two attacks, normal and special.
+ * The user is able to control this character. This class inherits properties from
+ * the Entity class.
+ * @author Oliver Frank
+ */
+public class Protagonist extends Character{
+	
+	private int normalDEF;
+	private double normalATK;
+	
+	/**
+	 * The default constructor, it sets the HP, STR, DED, and ATK to
+	 * reasonable values. Name is kobold by default
+	 **/
+	public Protagonist() {
+ 		this.name = "pat";
+ 		this.hp = 125;
+ 		this.str = 100;
+ 		this.def = 40;
+ 		this.normalDEF = this.def;
+ 		this.atk = 0.4;
+ 		this.normalATK = this.atk;
+ 	}
+	
+	/**
+	 * Constructor that allows name of Entity to be set. Inherits other values
+	 * from default constructor
+	 **/
+	public Protagonist(String name) {
+ 		this();
+ 		this.name = name;
+ 	}
+
+ 	/**
+ 	 * halfs the defense by 2 times and increases the attack by 2 times.
+ 	 */
+ 	public void specialize() {
+ 		def /= 2;
+ 		atk *= 2;
+ 	}
+ 	
+ 	/**
+ 	 * Makes the defense and attack the default values again
+ 	 */
+ 	public void normalize() {
+ 		def = normalDEF;
+ 		atk = normalATK;
+ 	}
+} //end class
