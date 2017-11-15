e1bb1498ffcd6ca82a53afcd3917550dbda756a7
kevin feng <kevinfeng12345@gmail.com>
2017-11-15 17:00:40 -0500

    Added show health and spawn random monster functionality

---
+++ v4/Pyro.java
@@ -0,0 +1,35 @@
+/*
+ * The Knowledgeable Octocats - Oliver Frank, Shayan Chowdhury, Kevin Feng
+ * APCS pd02
+ * HW28 -- Ye Olde Role Playing Game, Expanded
+ * 2017-11-13
+ **/
+
+/**
+ * Pickable subclass of Protagonist. Has low hp, high attack, low defense.
+ * 
+ **/
+public class Pyro extends Monster {
+     /**
+     * Default Constructor for class.  Sets name to Scout by default.
+     **/
+    public Pyro () {
+        _name = "Pyro";
+        _hitPts = 200;
+        _strength = 80;
+        _defense = 30;
+        _attack = .8;
+    }
+
+      /**
+     * 1-parameter overloaded constructor.  Allows name to be set to user's choice.
+     **/
+    public Pyro (String name) {
+        this();
+        _name = name;
+    }
+
+    public String about() {
+        return "Hey you, yeah you. I'm talkin to ya. You know who I am? I'm the most beautiful thing you're ever gonna see in your life, pal. I'm the Scout... and don't you ever forget it.";
+    }
+}
